042f2316f3e4fdb9d95cc397fbc7e6f349b74c96
Eduard <eduardpovazhnyi@example.com>
2024-10-22 11:55:01 +0100

    update

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>This is the largest header</h1>
+    <h2>Second largest header</h2>
+    <h3>Thirsd largest header</h3>
+    <h4>Fourth largest header</h4>
+    <h5>Fifth largest header</h5>
+    <h6>Sixth largest header</h6>
+    <p>This is a paragraph</p>
+
+    <!-- Adding an external link -->
+    <a href="https://google.com">Link to google</a>
+
+    <!-- Creating a table -->
+    <table>
+        <thead>
+            <tr>
+                <td>Column One</td>
+                <td>Colums Two</td>
+                <td>Colums Three</td>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+                <td>Column One data</td>
+                <td>Colums Two data</td>
+                <td>Colums Three data</td>
+            </tr>
+
+        </tbody>
+      </table>
+
+
+
+</body>
+</html>
